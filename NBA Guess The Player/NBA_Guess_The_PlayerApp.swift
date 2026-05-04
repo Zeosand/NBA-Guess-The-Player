4724b1b787079cf8e458ddc80ab0f3aa4ca53330
BT_09_09 <mobileappdev@bths.edu>
2026-05-04 14:42:39 -0400

    Initial Commit

---
+++ NBA Guess The Player/NBA_Guess_The_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_Guess_The_PlayerApp.swift
+//  NBA Guess The Player
+//
+//  Created by Student on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct NBA_Guess_The_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
